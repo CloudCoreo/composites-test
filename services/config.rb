a3c5774085d52f6fb5af6491412604fcd4ff8e50
David Mattes <mattes@nixnux.org>
2017-01-11 17:05:54 -0800

    Add jsrunner with syntax error

--- services/config.rb
+++ services/config.rb
@@ -1,6 +1,10 @@
-coreo_aws_vpc_vpc "${VPC_NAME}" do
-  action :find
-  cidr "${VPC_OCTETS}"
-  region "${REGION}"
-  tags ${VPC_TAGS}
+# The following resource is expected to fail
+coreo_uni_util_jsrunner "jsrunner-script-fails-always-runs" do
+  action :run
+  trigger "1"
+  json_input '{ "hi script error": [ {"this": "resource"}, {"always": "runs"} ] }'
+  function <<-EOH
+console.log('we are running');
+callback(json_input["hi trigger");
+  EOH
 end
